71b2edbaa1a435f3ba4f62e50de20615e21da77f
Gabriel <gabriel.sdiniz@outlook.com>
2024-07-13 17:25:21 -0300

    Feat: Create Themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { GlobalStyle, lighTheme } from './styles/'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lighTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
